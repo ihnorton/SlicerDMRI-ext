5ea8a453257971af0738c2e4903a900087214509
Isaiah Norton <isaiah.norton@gmail.com>
2016-01-05 15:05:49 -0500

    Fix spaces

--- TestExt-build.sh
+++ TestExt-build.sh
@@ -7,10 +7,10 @@ export GENERATOR="-GNinja"
 export MAKE="ninja -j3"
 
 cmake $GENERATOR \
-	    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
-	    -DSlicer_DIR:PATH=$SLICER_DIR \
-		-DSlicer_EXTENSION_DESCRIPTION_DIR:PATH=./ \
-	    -DCMAKE_BUILD_TYPE:STRING=Release \
-	    ${SLICER}/Extensions/CMake
+        -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
+        -DSlicer_DIR:PATH=$SLICER_DIR \
+        -DSlicer_EXTENSION_DESCRIPTION_DIR:PATH=./ \
+        -DCMAKE_BUILD_TYPE:STRING=Release \
+        ${SLICER}/Extensions/CMake
 
 $MAKE
