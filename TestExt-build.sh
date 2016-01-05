5be622509d4285a0aa733d10b926e04f2116b2a6
Isaiah Norton <isaiah.norton@gmail.com>
2016-01-05 13:25:16 -0500

    Test build

---
+++ TestExt-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mkdir TestExt-build
+cd TestExt-build
+
+export SLICER_DIR=~/git/slcr/s4
+export GENERATOR="-GNinja"
+export MAKE="ninja -j3"
+
+cmake $GENERATOR \
+	    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
+	    -DSlicer_DIR:PATH=$SLICER_DIR \
+		-DSlicer_EXTENSION_DESCRIPTION_DIR:PATH=./ \
+	    -DCMAKE_BUILD_TYPE:STRING=Release \
+	    ${SLICER}/Extensions/CMake
+
+$MAKE
